d06df57ae7e18377c95018a519bd99697998ad28
José Danubio de Araujo <danubio.bwm@gmail.com>
2026-08-07 11:28:51 -0300

    Cond BrodCast

---
+++ SincronizacaoAvancada/CondBroadCast/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Trava struct {
+	liberada bool
+	mu       sync.Mutex
+	cond     *sync.Cond
+}
+
+func NovaTrava() *Trava {
+	t := &Trava{}
+	t.cond = sync.NewCond(&t.mu)
+	return t
+}
+
+var start = time.Now()
+
+func logf(msg string) {
+	fmt.Printf("[%4dm] %s\n", time.Since(start).Microseconds(), msg)
+}
+
+func worker(id int, t *Trava, wg *sync.WaitGroup) {
+	defer wg.Done()
+	logf(fmt.Sprintf("Worker %d: aguardando liberação da trava", id))
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	for !t.liberada {
+		logf(fmt.Sprintf("Worker %d: trava não liberada, aguardando...", id))
+		t.cond.Wait()
+	}
+	logf(fmt.Sprintf("Worker %d: trava liberada, continuando execução", id))
+	time.Sleep(time.Millisecond * 100)
+}
+
+func main() {
+
+	var (
+		qtdeWorkers = 5
+		trava       = NovaTrava()
+		wg          sync.WaitGroup
+	)
+
+	wg.Add(qtdeWorkers)
+
+	for i := 0; i < qtdeWorkers; i++ {
+		go worker(i, trava, &wg)
+	}
+	time.Sleep(time.Microsecond * 500)
+	trava.mu.Lock()
+	trava.liberada = true
+	logf("Main: liberando trava e notificando todos os workers")
+	trava.cond.Broadcast()
+	trava.mu.Unlock()
+	wg.Wait()
+
+}
